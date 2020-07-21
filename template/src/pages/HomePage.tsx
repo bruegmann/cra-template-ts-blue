139a8e97eba9f9be11f87f186ba06ec31fdbf7c9
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-07-21 09:34:27 +0200

    Support for strict TS and functional component * The default HomePage component is now functional

--- template/src/pages/HomePage.tsx
+++ template/src/pages/HomePage.tsx
@@ -3,27 +3,25 @@ import { Page, Header, HeaderTitle, Body } from "blue-react";
 
 import { appLogo, appTitle, getPhrase as _ } from "../shared";
 
-class HomePage extends React.Component {
-    render() {
-        return (
-            <Page>
-                <Header>
-                    <HeaderTitle logo={appLogo} appTitle={appTitle}>HomePage</HeaderTitle>
-                </Header>
+function HomePage() {
+    return (
+        <Page>
+            <Header>
+                <HeaderTitle logo={appLogo} appTitle={appTitle}>HomePage</HeaderTitle>
+            </Header>
 
-                <Body containerClass="container">
-                    <h1 className="mt-4 mb-3">{_("HELLO_WORLD")}</h1>
+            <Body containerClass="container">
+                <h1 className="mt-4 mb-3">{_("HELLO_WORLD")}</h1>
 
-                    <button
-                        className="btn btn-lg btn-primary"
-                        onClick={() => alert(_("HELLO_WORLD"))}
-                    >
-                        Why don't you click this
+                <button
+                    className="btn btn-lg btn-primary"
+                    onClick={() => alert(_("HELLO_WORLD"))}
+                >
+                    Why don't you click this
                     </button>
-                </Body>
-            </Page>
-        );
-    }
+            </Body>
+        </Page>
+    );
 }
 
 export default HomePage;
